db69a568f5e6480f5340b25b3e7343e7e0bcc774
MasterUNG <phrombutr@gmail.com>
2018-03-02 11:20:44 +0700

    Check Space OK

--- app/src/main/java/masterung/androidthai/in/th/onofflight/fragment/AuthenFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/onofflight/fragment/AuthenFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import masterung.androidthai.in.th.onofflight.R;
 
@@ -14,6 +17,40 @@ import masterung.androidthai.in.th.onofflight.R;
  */
 
 public class AuthenFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Login Controller
+        loginController();
+
+
+    }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Space
+                    Toast.makeText(getActivity(), "Please Fill All Blank", Toast.LENGTH_SHORT).show();
+                } else {
+//                    No Space
+
+                }
+
+            }
+        });
+    }
 
     @Nullable
     @Override
